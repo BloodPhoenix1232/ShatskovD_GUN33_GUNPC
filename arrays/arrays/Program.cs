fd90c31a8dc5df1cd4b78ae7024814015b446aab
Шацков Дмитрий <dima.shaczkov@mail.ru>
2024-10-31 13:46:33 +0300

    add HomeWork2

---
+++ arrays/arrays/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace HomeWork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Здесь массивы заданий 1-4
+
+            // массивы для заданий 5 и 6.
+            int[] array = { 1, 2, 3, 4, 5 };
+            int[] array2 = { 7, 8, 9, 10, 11, 12, 13 };
+            var result = CopyArrays(array, array2, 2);
+            // Выведите результат
+
+            string[] sample = { "", "" };
+            ResizeArray(ref array, /* подставьте число вторым аргументов  */ );
+            // Что же будет выведено?
+        }
+    }
+}
